58f6f08a88731b6a5e1505110f2493b127ff0049
Ebenezer Ademola <ebenezeradem@gmail.com>
2021-03-10 19:06:13 -0500

    Added fade-in effect

--- script.js
+++ script.js
@@ -12,40 +12,28 @@ let weather = {
     },
     displayWeather: function(data) {
         const { name } = data;
-        const { country, sunrise, sunset } = data.sys
+        const { country} = data.sys
         const { icon, description } = data.weather[0];
         const { temp, humidity, feels_like } = data.main;
         const { speed } = data.wind;
-        console.log(name, country, sunrise, sunset, icon, description, feels_like, temp, humidity, speed)
+        console.log(name, country, icon, description, feels_like, temp, humidity, speed);
         document.querySelector(".city").innerText = `${name}, ${country}`;
-        //document.querySelector(".sunrise-txt").innerText = ` ${sunrise}`;
-        //document.querySelector(".sunset-txt").innerText = ` ${sunset}`;
         document.querySelector(".temp").innerText = `${Math.round(temp)}°F`;
         document.querySelector(".icon").src = `http://openweathermap.org/img/wn/${icon}@2x.png`
         document.querySelector(".description").innerText = `${description}`;
         document.querySelector(".feel").innerText = `Feels like ${Math.round(feels_like)}°F`
         document.querySelector(".humidity-txt").innerText = `${humidity}%`;
         document.querySelector(".wind-txt").innerText = `${speed} mph`;
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}, ${description}')`;
-        
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".landing").style.display = "none";
+        document.querySelector(".card").classList.add("fade");
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}-${description}')`;
     },
     search: function () {
         this.fetchWeather(document.querySelector(".searchbar").value);
+        document.querySelector(".card").classList.remove("fade");
     },
-    
 }
-
-// function suntime(x) {
-//     let dateObj = new Date(x * 1000);
-
-//     let hours = dateObj.getUTCHours.toString.padStart(2,0);
-//     let minutes = dateObj.getUTCMinutes.toString.padStart(2,0);
-//     let seconds = dateObj.getUTCSeconds.toString.padStart(2,0);
-
-//     console.log(` Time is ${hours}:${minutes}:${seconds}`);
-//     return `${hours}:${minutes}:${seconds}`;
-// }
 
 document.querySelector(".search button").addEventListener('click', function () {
      weather.search();
